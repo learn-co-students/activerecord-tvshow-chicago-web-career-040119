8b08be1161b12199710fec4ddefa2db7302b5427
github username <github email address>
2019-04-12 15:21:36 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,12 @@ Bundler.require
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
-
+puts "about to establish connection"
 DB = ActiveRecord::Base.establish_connection({
   adapter: 'sqlite3',
   database: 'db/tvshows.db'
 })
+puts "made it past connection setup"
 
 if ENV["ACTIVE_RECORD_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
